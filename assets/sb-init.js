68ec2e98edf75de6eb5b32344c3534a2ac82f947
James <jacopoberton98@gmail.com>
2025-09-26 23:15:12 +0200

    Update sb-init.js

--- assets/sb-init.js
+++ assets/sb-init.js
@@ -1,16 +1,3 @@
-// assets/sb-init.js
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./sb-config.js";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
-});
-
-// IMPORTANT: access-mode.js waits for window.$sb
-window.__SB = sb;
-window.$sb  = sb;
-
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './sb-config.js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
