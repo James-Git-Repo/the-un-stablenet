c864346ff62c061e5743376625b42d7bc074aba0
James <jacopoberton98@gmail.com>
2025-09-26 11:12:01 +0200

    Update sb-init.js

--- assets/sb-init.js
+++ assets/sb-init.js
@@ -1,6 +1,6 @@
 // assets/sb-init.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./sb-config.js";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
